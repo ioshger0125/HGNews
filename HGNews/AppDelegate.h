1df1c93af801bef05217c4ba523d7006a31be009
Hua Guo <hglcc0125@hotmail.com>
2016-05-09 22:38:51 +0800

    Initial Commit

---
+++ HGNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HGNews
+//
+//  Created by HuaGuo on 16/5/9.
+//  Copyright © 2016年 HG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HGNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGNews
+//
+//  Created by HuaGuo on 16/5/9.
+//  Copyright © 2016年 HG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
